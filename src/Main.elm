56daa3aaaa9919a3c5e724463cfb6bd08f50f98b
Jan Hrček <honza.hrk@gmail.com>
2022-02-06 11:28:04 +0100

    Use keyboard subscription instead of input field

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,9 @@
 module Main exposing (main)
 
 import Browser exposing (Document)
-import Html
+import Html exposing (Html)
 import Html.Attributes as A
-import Html.Events as E
+import Keyboard exposing (RawKey)
 
 
 main : Program () Model Msg
@@ -19,19 +19,19 @@ main =
 type alias Model =
     { acceptedChars : List Char
     , remainingChars : List Char
-    , wordInput : String
+    , mistypedChars : List Char
     }
 
 
 type Msg
-    = InputUpdated String
+    = KeyDown RawKey
 
 
 init : () -> ( Model, Cmd Msg )
 init () =
-    ( { acceptedChars = []
-      , remainingChars = String.toList "But in a solitary life, there are rare moments when another soul dips near yours, as stars once a year brush the earth. Such a constellation was he to me."
-      , wordInput = ""
+    ( { remainingChars = String.toList "But in a solitary life, there are rare moments when another soul dips near yours, as stars once a year brush the earth. Such a constellation was he to me."
+      , acceptedChars = []
+      , mistypedChars = []
       }
     , Cmd.none
     )
@@ -47,63 +47,82 @@ view model =
             , A.style "font-weight" "500"
             ]
             (Html.span [ A.style "color" "#95c590" ] [ Html.text (String.fromList model.acceptedChars) ]
-                :: (case model.remainingChars of
+                :: (case model.mistypedChars of
                         [] ->
-                            []
+                            remainingCharsView model.remainingChars
 
-                        c :: rest ->
-                            [ Html.span
-                                [ A.style "color" "#3295db"
-                                , A.style "text-decoration" "underline"
+                        _ ->
+                            Html.span
+                                [ A.style "color" "#803333"
+                                , A.style "background-color" "#f0a3a3"
                                 ]
-                                [ Html.text (String.fromChar c) ]
-                            , Html.span [] [ Html.text (String.fromList rest) ]
-                            ]
+                                [ Html.text (String.fromList model.mistypedChars) ]
+                                :: (remainingCharsView <| List.drop (List.length model.mistypedChars) model.remainingChars)
                    )
             )
         , Html.div []
-            [ Html.input
-                [ A.type_ "text"
-                , E.onInput InputUpdated
-                , A.value model.wordInput
-                ]
-                []
-            ]
+            [ Html.text <| String.fromList model.mistypedChars ]
         ]
     }
+
+
+remainingCharsView : List Char -> List (Html Msg)
+remainingCharsView chars =
+    case chars of
+        [] ->
+            []
+
+        c :: rest ->
+            [ Html.span
+                [ A.style "color" "#3295db"
+                , A.style "text-decoration" "underline"
+                ]
+                [ Html.text (String.fromChar c) ]
+            , Html.span [] [ Html.text (String.fromList rest) ]
+            ]
+
+
+keyParser =
+    Keyboard.oneOf
+        [ Keyboard.characterKeyOriginal
+        , Keyboard.editingKey
+        ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        InputUpdated str ->
-            case ( List.head <| List.reverse <| String.toList str, model.remainingChars ) of
-                ( Just a, b :: rest ) ->
-                    if a == b then
-                        ( { model
-                            | acceptedChars = model.acceptedChars ++ [ a ]
-                            , remainingChars = rest
-                            , wordInput =
-                                if a == ' ' then
-                                    ""
+        KeyDown rawKey ->
+            case keyParser rawKey of
+                Just Keyboard.Backspace ->
+                    ( { model | mistypedChars = List.take (List.length model.mistypedChars - 1) model.mistypedChars }
+                    , Cmd.none
+                    )
 
-                                else
-                                    model.wordInput ++ String.fromChar a
-                          }
-                        , Cmd.none
-                        )
+                Just (Keyboard.Character c) ->
+                    case model.remainingChars of
+                        b :: rest ->
+                            if c == String.fromChar b then
+                                ( { model
+                                    | acceptedChars = model.acceptedChars ++ String.toList c
+                                    , remainingChars = rest
+                                  }
+                                , Cmd.none
+                                )
 
-                    else
-                        -- TODO display bad chars and allow backspacing them
-                        ( model, Cmd.none )
+                            else
+                                ( { model | mistypedChars = model.mistypedChars ++ String.toList c }
+                                , Cmd.none
+                                )
 
-                ( Nothing, _ ) ->
+                        [] ->
+                            ( model, Cmd.none )
+
+                _ ->
+                    -- TODO do we need to handle other chars?
                     ( model, Cmd.none )
-
-                ( _, [] ) ->
-                    ( { model | wordInput = "" }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
+subscriptions _ =
+    Keyboard.downs KeyDown
